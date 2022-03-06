ebbc43721e1a8e41bfad9564436629cf3bb7e38f
Daniel Żyła <dzmsgbox@gmail.com>
2022-03-06 01:34:50 +0100

    createFile method extracted to new interface FileHandler

--- src/main/java/io/github/danielzyla/article/ArticleFileHandler.java
+++ src/main/java/io/github/danielzyla/article/ArticleFileHandler.java
@@ -1,22 +1,19 @@
 package io.github.danielzyla.article;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import io.github.danielzyla.utils.FileHandler;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
-import java.util.logging.Logger;
 
-class ArticleFileHandler {
+class ArticleFileHandler implements FileHandler {
 
     private static ArticleFileHandler fileHandler;
-    protected final Logger log = Logger.getLogger(ArticleFileHandler.class.getName());
-    static File articleDataFile;
 
     private ArticleFileHandler() throws IOException {
-        createFile();
+        createFile("articlesData.txt");
     }
 
     public static ArticleFileHandler getInstance() throws IOException {
@@ -24,17 +21,6 @@ class ArticleFileHandler {
             fileHandler = new ArticleFileHandler();
         }
         return fileHandler;
-    }
-
-    private void createFile() throws IOException {
-        articleDataFile = new File("articlesData.txt");
-        if (!articleDataFile.exists()) {
-            boolean isNewFileCreated = articleDataFile.createNewFile();
-            log.info("new articleDataFile creation status: " + isNewFileCreated);
-        } else {
-            log.info("articleDataFile exists");
-            new FileOutputStream("articlesData.txt").close();
-        }
     }
 
     void saveArticlesToFile(final HashSet<ArticleDto> articleSet) {
